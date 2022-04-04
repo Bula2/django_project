bdedd6fdcb9fd8e62542b4c5d27210fdb92eb777
Bula2 <bulaev123da@gmail.com>
2022-04-04 19:42:55 +0300

    Updates

--- django_project/web/news/views.py
+++ django_project/web/news/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+
 
 def news_home(request):
     news = Articles.objects.order_by("-data")[:2]
     return render(request, 'news/news_home.html', {'news' : news})
+
+class NewsDetailView(DetailView):
+    model= Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model=Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model=Articles
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+    context_object_name = 'article'
 
 def create(request):
     error = ''
@@ -12,7 +30,7 @@ def create(request):
         form=ArticlesForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('news')
         else:
             error = 'Форма заполнена неверно'
 
