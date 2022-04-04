03f9cac5a00ac068a2c0c9f2e2fea2c8f17b4b88
Bula2 <bulaev123da@gmail.com>
2022-04-04 14:43:36 +0300

    Updates

--- django_project/web/news/models.py
+++ django_project/web/news/models.py
@@ -5,7 +5,7 @@ class Articles(models.Model):
     title = models.CharField('Название', max_length=50, default='')
     anons = models.CharField('Анонс', max_length=250, default='')
     full_text = models.TextField('Статья')
-    data=models.DateTimeField('Дата публикации')
+    data = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return self.title
